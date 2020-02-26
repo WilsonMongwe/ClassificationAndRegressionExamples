1b9553457f08af3192d36a691b5bd9bf0280c3af
WilsonMongwe <wilsonmongwe@gmail.com>
2020-02-26 19:42:25 +0200

    updated results

--- source/main_iris.py
+++ source/main_iris.py
@@ -29,7 +29,7 @@ def log_likelihood(W, *logl_args):
 
 #Store results
 results_list = [] # to store result of nested sampling
-x_axis = range(1,31,1) # Number of hidden units
+x_axis = range(1,101,1) # Number of hidden units
 # Architecture for mlp
 input_neurons = x_train.shape[1] # inputs
 output_neurons = 3
